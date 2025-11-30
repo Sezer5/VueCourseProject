24f69ade6afc1d793067ba7f810962b722007832
Sezer5 <unalmissezer@gmail.com>
2025-11-30 15:08:34 +0300

    Proje için gerekli vuetify vee validate yup toast plugin bootstrap ve firebase eklentileri yapıldı proje hala boş

--- src/main.js
+++ src/main.js
@@ -5,9 +5,33 @@ import './assets/main.css'
 import App from './App.vue'
 import router from './router'
 
+// TOAST NOTIFICATION
+
+import ToastPlugin from 'vue-toast-notification';
+// Import one of the available themes
+//import 'vue-toast-notification/dist/theme-default.css';
+import 'vue-toast-notification/dist/theme-bootstrap.css';
+
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(vuetify)
+app.use(ToastPlugin)
 app.mount('#app')
