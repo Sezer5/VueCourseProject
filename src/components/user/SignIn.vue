fbca361c404f96cef0a5f4e60f478212dc1eceef
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-12-01 16:13:53 +0300

    Sign-in componenti açılışı ve kısmi olarak css verilmesi

---
+++ src/components/user/SignIn.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="signinContainer">
+        <Form>
+            <h1>Eğitmen Girişi</h1>
+            <div class="form-group">
+                <Field name="email" v-slot="{field,errors,errorMessage}">
+                    <input type="email" class="form-control" placeholder="Lütfen Mailinizi Giriniz." v-bind="field" />
+                    <div class="input-alert" v-if="errors.length!==0">
+                        {{ errorMessage }}
+                    </div>
+                </Field>
+            </div>
+            <div class="form-group">
+                <Field name="password" v-slot="{field,errors,errorMessage}">
+                    <input type="password" class="form-control" placeholder="Lütfen Şifrenizi Giriniz." v-bind="field" />
+                    <div class="input-alert" v-if="errors.length!==0">
+                        {{ errorMessage }}
+                    </div>
+                </Field>
+            </div>
+            <button class="btn btn-success btn-block">Giriş Yap</button>
+        </Form>
+    </div>
+</template>
+
+<script setup>
+import {Field,Form} from 'vee-validate';
+import * as yup from 'yup';
+</script>
+
+<style scoped>
+    
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/components/Home.vue'
+import SignIn from '@/components/user/SignIn.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -7,6 +8,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SignIn,
     },
     
   ],
