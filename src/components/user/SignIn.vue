1586021635a338cd9c64e31672e219fa48f75253
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-12-02 10:21:29 +0300

    Veevaliadete ve yup ile form validasyon işlemlerinin yapılması

--- src/components/user/SignIn.vue
+++ src/components/user/SignIn.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="signinContainer">
-        <Form>
+        <Form :validation-schema="formSchema">
             <h1 class="text-center">{{isLoggedScreen ? 'Eğitmen Girişi' : 'Kayıt Ol!'}}</h1>
             <div class="form-group">
                 <Field name="email" v-slot="{field,errors,errorMessage}">
-                    <input type="email" class="form-control" placeholder="Lütfen Mailinizi Giriniz." v-bind="field" />
+                    <input type="email" class="form-control" placeholder="Lütfen Mailinizi Giriniz." v-bind="field" :class="{'is-invalid':errors.length!==0}"/>
                     <div class="input-alert" v-if="errors.length!==0">
                         {{ errorMessage }}
                     </div>
@@ -12,7 +12,7 @@
             </div>
             <div class="form-group">
                 <Field name="password" v-slot="{field,errors,errorMessage}">
-                    <input type="password" class="form-control" placeholder="Lütfen Şifrenizi Giriniz." v-bind="field" />
+                    <input type="password" class="form-control" placeholder="Lütfen Şifrenizi Giriniz." v-bind="field" :class="{'is-invalid':errors.length!==0}"/>
                     <div class="input-alert" v-if="errors.length!==0">
                         {{ errorMessage }}
                     </div>
@@ -31,6 +31,14 @@
 import {Field,Form} from 'vee-validate';
 import * as yup from 'yup';
 import {ref} from 'vue';
+
+
+const formSchema = yup.object({
+    email:yup.string().required('Email girmek zorunludur').email('Lütfen doğru formatta email giriniz!'),
+    password:yup.string().required('Şifre girmek zorunludur')
+});
+
+
 
 const isLoggedScreen=ref(true);
 </script>
