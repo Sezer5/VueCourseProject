93c1a524e16b83cc16637f0fe7cd7beada48b018
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-12-03 14:05:07 +0300

    sidebar yapımı

--- src/components/user/dashboard/MainPage.vue
+++ src/components/user/dashboard/MainPage.vue
@@ -1,11 +1,46 @@
 <template>
-  deneme
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 bg-light d-block collapse sidebar">
+                <div>
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <router-link class="nav-link" :to="{name:'dashboard'}">
+                                <v-icon icon="mdi-view-dashboard"/>
+                                <span>Kullanıcı Sayfası</span>
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" :to="{name:'profile'}">
+                                <v-icon icon="mdi-account"/>
+                                <span>Eğitmen</span>
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" :to="{name:'courses'}">
+                                <v-icon icon="mdi-note-outline"/>
+                                <span>Kurslarım</span>
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" :to="{name:'courses_add'}">
+                                <v-icon icon="mdi-note-edit-outline"/>
+                                <span>Kurs Ekle</span>
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+
+            </nav>
+            <div class="col-md-10">
+                <router-view></router-view>
+            </div>
+
+
+        </div>
+        
+    </div>
 </template>
 
 <script setup>
-
 </script>
-
-<style>
-
-</style>
