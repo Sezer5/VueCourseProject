ef58bce6d126edbf68c39dc9fa770dd972fdf811
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-12-06 11:29:06 +0300

    Composable kullanarak eğitmen belgelerinini güncellenmesi

--- src/components/user/dashboard/Profile.vue
+++ src/components/user/dashboard/Profile.vue
@@ -1,10 +1,33 @@
 <template>
-    <div>
-        Profile
+    <div class="col-lg-4">
+        <Form :validation-schema="formSchema" @submit="onSubmit">
+            <h1 class="text-center">Eğitmen Bilgileri</h1>
+            <div class="mb-4">
+                <Field name="firstname" v-slot="{field,errors,errorMessage}">
+                    <input type="text" class="form-control" placeholder="Lütfen Adınızı Giriniz." v-bind="field" :class="{'is-invalid':errors.length!==0}"/>
+                    <div class="input-alert" v-if="errors.length!==0">
+                        {{ errorMessage }}
+                    </div>
+                </Field>
+            </div>
+            <div class="mb-4">
+                <Field name="lastname" v-slot="{field,errors,errorMessage}">
+                    <input type="text" class="form-control" placeholder="Lütfen Soyadınızı Giriniz." v-bind="field" :class="{'is-invalid':errors.length!==0}"/>
+                    <div class="input-alert" v-if="errors.length!==0">
+                        {{ errorMessage }}
+                    </div>
+                </Field>
+            </div>
+            <div class="mb-4 text-end">
+                <v-btn type="submit" variant="outlined">Güncelle</v-btn>
+            </div>
+            
+        </Form>
     </div>
 </template>
 
 <script setup>
+import {Field,Form} from 'vee-validate';
 
 </script>
 
